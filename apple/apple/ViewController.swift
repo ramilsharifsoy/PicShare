a0ba2524f5483eb3742df3ff787d5b90a179a0ef
Ramil Sharifsoy <ramil.sharifsoy@gmail.com>
2017-06-06 13:49:13 -0500

    Initiate Project apple

---
+++ apple/apple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apple
+//
+//  Created by FutugraF on 6/6/17.
+//  Copyright © 2017 FutugraF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
